13bf4d45551630abb60e23b25573a04a39447d53
jimmy <jimmiparekh936@gmail.com>
2026-08-04 10:24:50 +0530

    Implement user registration API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const authRouter =require("./routes/authRoutes");
+const authRouter = require("./routes/authRoutes");
 const healthRoutes = require("./routes/health.routes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/app/auth", authRouter);
-app.use("/api/v1/health", healthRoutes);
+app.use("/api/auth", authRouter);
+// app.use("/api/v1/health", healthRoutes);
 
 module.exports = app;
